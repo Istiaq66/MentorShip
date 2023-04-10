6bd2229128204dd4bf3bcabc980fa72a9b8ac22e
Isitaq66 <ahmedboby66@gmail.com>
2023-04-10 23:33:50 +0600

    I'm_rich-->10/04/23

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,3 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: const Text('Hello')));
+void main() {
+  runApp(
+    MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Center(
+          child: Text("I'm Rich"),
+        ),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/diamond.png'),
+        ),
+      ),
+    )),
+  );
+}
